d79a7b44da84f3c03f0d303fb0a6370063a95ce9
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-03-30 21:27:09 +0200

    added streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Load data
+
+st.title('Which Business to run in Barcelona')
+selected_task = st.sidebar.selectbox('Menu instead!!!!', ['Restaurant', 'Hotels', 'Pubs', 'Business comparison', 'Recommendation'])
+
+st.write('Photo of Barcola in the background')
+
+
+st.write('Some text about the project and key facts about barcelona')
+
+
+
