bc2a5073bb1398fb035009d4f4888854b0b6cdb9
“sardorick” <sardorzakirov1999@gmail.com>
2023-01-06 11:18:04 +0100

    added ETL pipeline to save data to database

---
+++ API to save to SQL/request.py
@@ -0,0 +1,17 @@
+import requests
+from auth import headers
+import json
+
+class Request:
+    def __init__(self):
+        self._header = headers
+
+    @staticmethod
+    def get_content(url, param):
+        response = requests.get(url, headers=headers, params=param)
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            print(f"Response completed with error. Response code: {response.status_code}")
+            return None
+
